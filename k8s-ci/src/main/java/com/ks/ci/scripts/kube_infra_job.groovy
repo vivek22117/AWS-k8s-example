7e4f103c45167b405fd8f9788c794898506d5551
vivek22117 <vivekmishra22117@gmail.com>
2020-10-17 22:29:20 +0530

    Adding auth config file for aws-auth configurtion

---
+++ k8s-ci/src/main/java/com/ks/ci/scripts/kube_infra_job.groovy
@@ -0,0 +1,52 @@
+package com.ks.ci.scripts
+
+import com.ks.ci.builder.KubeInfraBuilder
+import javaposse.jobdsl.dsl.JobParent
+
+
+def factory = this as JobParent
+def listOfEnvironment = ["dev", "qa", "prod"]
+def component = "kube-infra-job"
+
+def emailId = "vivekmishra22117@gmail.com"
+def description = "Pipeline DSL to AWS EKS Infra!"
+def displayName = "Kubernetes AWS Infra Job"
+def branchesName = "*/master"
+def githubUrl = "https://github.com/vivek22117/AWS-k8s-example.git"
+
+
+new KubeInfraBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(0),
+        displayName: displayName + " " + listOfEnvironment.get(0),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(0),
+        emailId: emailId
+).build()
+
+new KubeInfraBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(1),
+        displayName: displayName + " " + listOfEnvironment.get(1),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(1),
+        emailId: emailId
+).build()
+
+new KubeInfraBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(2),
+        displayName: displayName + " " + listOfEnvironment.get(2),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(2),
+        emailId: emailId
+).build()
