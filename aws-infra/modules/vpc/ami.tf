9a89a7c6e0beef3d7469cd2dee21e9de7f9756d6
vivek22117 <vivekmishra22117@gmail.com>
2021-06-17 21:11:19 +0530

    adding variables for TF

--- aws-infra/modules/vpc/ami.tf
+++ aws-infra/modules/vpc/ami.tf
@@ -10,3 +10,15 @@ data "aws_ami" "bastion" {
     values = ["eks-bastion"]
   }
 }
+
+
+#####===========================As Of Now Not Used=================================#####
+data "aws_ami" "eks-worker" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${aws_eks_cluster.doubledigit_eks.version}-v*"]
+  }
+
+  most_recent = true
+  owners      = ["602401143452"] # Amazon EKS AMI Account ID
+}
